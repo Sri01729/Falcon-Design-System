a637a40e4a634ecbbf1b839bdb08de7295aaee1f
Sri01729 <salahari@northislandcollege.ca>
2023-04-19 00:59:06 -0700

    Following changes were made
    1. Added BMI calculator button function using javascript.
    2. Added website heading animation using javascript.

--- Javascript/appp.js
+++ Javascript/appp.js
@@ -1,4 +1,73 @@
 "use strict";
+// BMI calculator function
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onload = () => {
+	let button = document.querySelector("#btn");
+
+	// Function for calculating BMI
+	button.addEventListener("click", calculateBMI);
+};
+
+function calculateBMI() {
+    console.log("Button clicked");
+	/* Getting input from user into height variable.
+	Input is string so typecasting is necessary. */
+	let height = parseInt(document
+			.querySelector("#height").value);
+
+	/* Getting input from user into weight variable.
+	Input is string so typecasting is necessary.*/
+	let weight = parseInt(document
+			.querySelector("#weight").value);
+
+	let result = document.querySelector("#result");
+
+	// Checking the user providing a proper
+	// value or not
+	if (height === "" || isNaN(height))
+		result.innerHTML = "Provide a valid Height!";
+
+	else if (weight === "" || isNaN(weight))
+		result.innerHTML = "Provide a valid Weight!";
+
+	// If both input is valid, calculate the bmi
+	else {
+
+		// Fixing upto 2 decimal places
+		let bmi = (weight / ((height * height)
+							/ 10000)).toFixed(2);
+
+		// Dividing as per the bmi conditions
+		if (bmi < 18.6) result.innerHTML =
+			`Under Weight : <span>${bmi}</span>`;
+
+		else if (bmi >= 18.6 && bmi < 24.9)
+			result.innerHTML =
+				`Normal : <span>${bmi}</span>`;
+
+		else result.innerHTML =
+			`Over Weight : <span>${bmi}</span>`;
+	}
+}
+
 
 let result;
 /*  Search list function */
@@ -109,22 +178,25 @@ const Confirm = {
     }
 };
 
-const nameInput = document.getElementById('name');
-const emailInput = document.getElementById('email');
-const messageInput = document.getElementById('message');
-const submitBtn = document.getElementById('submit');
 
-nameInput.addEventListener('input', validateForm);
-emailInput.addEventListener('input', validateForm);
-messageInput.addEventListener('input', validateForm);
 
-function validateForm() {
-  if (nameInput.value.trim() !== '' && emailInput.value.trim() !== '' && messageInput.value.trim() !== '') {
-    submitBtn.disabled = false;
-  } else {
-    submitBtn.disabled = true;
-  }
-}
+// Website title animation
 
+// This line creates a variable 'string' and assigns it a string value "IRON SANCTUARY".
+var string = "IRON SANCTUARY";
+
+// This line creates a variable 'str' and uses the split() method to convert the string 'string' into an array of characters.
+var str = string.split("");
+
+// This line retrieves an HTML element with the ID 'str' and assigns it to the variable 'el'.
+var el = document.getElementById('str');
+
+// This is a self-invoking function that uses a setTimeout() method to create an animation effect by continuously adding characters from the 'str' array to the innerHTML of the 'el' element.
+(function animate() {
+  // This conditional statement checks if there are any characters left in the 'str' array. If there are, it adds the first character to the innerHTML of the 'el' element, and then removes it from the 'str' array using the shift() method. If there aren't any characters left in the 'str' array, it clears the timeout using clearTimeout() method to stop the animation.
+  str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running);
+  // This line creates a timeout using the setTimeout() method, which invokes the 'animate' function after a delay of 90 milliseconds and assigns it to the variable 'running'.
+  var running = setTimeout(animate, 90);
+})();
 
 
